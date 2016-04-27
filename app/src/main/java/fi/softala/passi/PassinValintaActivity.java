bbbf17028e5b109038e98cd9e1b9f16d273e81d9
Kristian <kristian.harju@myy.haaga-helia.fi>
2016-04-27 14:40:21 +0200

    Passin valinta -Kristian

--- app/src/main/java/fi/softala/passi/PassinValintaActivity.java
+++ app/src/main/java/fi/softala/passi/PassinValintaActivity.java
@@ -1,7 +1,10 @@
 package fi.softala.passi;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PassinValintaActivity extends ActionBarActivity {
 
@@ -9,5 +12,13 @@ public class PassinValintaActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passin_valinta);
+        Button button = (Button) findViewById(R.id.btJotain);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(PassinValintaActivity.this, TehtavakorttiNostoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
