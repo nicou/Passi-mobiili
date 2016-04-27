42fb5659e969fabc19c9e0d663e3772d620d15ae
Kristian <kristian.harju@myy.haaga-helia.fi>
2016-04-27 19:19:32 +0200

    Nappeja -Kristian

--- app/src/main/java/fi/softala/passi/PassinValintaActivity.java
+++ app/src/main/java/fi/softala/passi/PassinValintaActivity.java
@@ -12,8 +12,40 @@ public class PassinValintaActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passin_valinta);
-        Button button = (Button) findViewById(R.id.btJotain);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button button1 = (Button) findViewById(R.id.btPassi1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(PassinValintaActivity.this, TehtavakorttiNostoActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.btPassi2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(PassinValintaActivity.this, TehtavakorttiNostoActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button3 = (Button) findViewById(R.id.btPassi3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(PassinValintaActivity.this, TehtavakorttiNostoActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button4 = (Button) findViewById(R.id.btPassi4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(PassinValintaActivity.this, TehtavakorttiNostoActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button5 = (Button) findViewById(R.id.btPassi5);
+        button5.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 Intent intent = new Intent(PassinValintaActivity.this, TehtavakorttiNostoActivity.class);
