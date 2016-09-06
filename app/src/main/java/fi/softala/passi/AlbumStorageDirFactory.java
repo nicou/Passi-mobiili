c181fd78922dd8768bf17e7de2e4943fdd63b9c8
Ville Aaltonen <villeaaltonen@Villes-MacBook-Pro.local>
2016-09-06 13:05:44 +0300

    Adding camera testing

---
+++ app/src/main/java/fi/softala/passi/AlbumStorageDirFactory.java
@@ -0,0 +1,7 @@
+package fi.softala.passi;
+
+import java.io.File;
+
+abstract class AlbumStorageDirFactory {
+	public abstract File getAlbumStorageDir(String albumName);
+}
--- app/src/main/java/fi/softala/passi/ValikkoActivity.java
+++ app/src/main/java/fi/softala/passi/ValikkoActivity.java
@@ -1,18 +1,32 @@
 package fi.softala.passi;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ValikkoActivity extends ActionBarActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_valikko);
         Button button = (Button) findViewById(R.id.btTyokykypassiin);
+        Button cameraButton = (Button) findViewById(R.id.btCameraTest);
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
@@ -24,5 +38,18 @@ public class ValikkoActivity extends ActionBarActivity {
 //                startActivity(intent);
             }
         });
-    }
+
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ValikkoActivity.this, Camera2Activity.class);
+                startActivity(intent);
+
+            }
+
+
+    });
+
+}
+
 }
