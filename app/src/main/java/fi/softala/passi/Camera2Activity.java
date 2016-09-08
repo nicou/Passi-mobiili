de0de93b9437609ff32c9318c29886e42d58cd48
Ville Aaltonen <villeaaltonen@Villes-MacBook-Pro.local>
2016-09-08 10:15:22 +0300

    Add fake notification for camera upload

--- app/src/main/java/fi/softala/passi/Camera2Activity.java
+++ app/src/main/java/fi/softala/passi/Camera2Activity.java
@@ -1,22 +1,26 @@
 package fi.softala.passi;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Build;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat.Builder;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.VideoView;
+import android.util.Log;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -26,8 +30,6 @@ import java.util.List;
 
 public class Camera2Activity extends Activity {
     private static final int ACTION_TAKE_PHOTO_B = 1;
-    private static final int ACTION_TAKE_PHOTO_S = 2;
-    private static final int ACTION_TAKE_VIDEO = 3;
 
     private static final String BITMAP_STORAGE_KEY = "viewbitmap";
     private static final String IMAGEVIEW_VISIBILITY_STORAGE_KEY = "imageviewvisibility";
@@ -46,6 +48,10 @@ public class Camera2Activity extends Activity {
 
     private AlbumStorageDirFactory mAlbumStorageDirFactory = null;
 
+    private NotificationManager mNotifyManager;
+    private Builder mBuilder;
+    int id = 1;
+
 
     /* Photo album for this application */
     private String getAlbumName() {
@@ -61,8 +67,8 @@ public class Camera2Activity extends Activity {
             storageDir = mAlbumStorageDirFactory.getAlbumStorageDir(getAlbumName());
 
             if (storageDir != null) {
-                if (! storageDir.mkdirs()) {
-                    if (! storageDir.exists()){
+                if (!storageDir.mkdirs()) {
+                    if (!storageDir.exists()) {
                         Log.d("CameraSample", "failed to create directory");
                         return null;
                     }
@@ -93,10 +99,67 @@ public class Camera2Activity extends Activity {
         return f;
     }
 
+    private void startUpload(String path) {
+        File fol = new File(path);
+        Log.v("TAGI", "usd" + path);
+        mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mBuilder = new NotificationCompat.Builder(Camera2Activity.this);
+        mBuilder.setContentTitle("Download")
+                .setContentText("Download in progress")
+                .setSmallIcon(R.drawable.ic_cloud_upload_white_24dp);
+
+        new Downloader().execute();
+    }
+
+    private class Downloader extends AsyncTask<Void, Integer, Integer> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            // Displays the progress bar for the first time.
+            mBuilder.setProgress(100, 0, false);
+            mNotifyManager.notify(id, mBuilder.build());
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            // Update progress
+            mBuilder.setProgress(100, values[0], false);
+            mNotifyManager.notify(id, mBuilder.build());
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            int i;
+            for (i = 0; i <= 100; i += 5) {
+                // Sets the progress indicator completion percentage
+                publishProgress(Math.min(i, 100));
+                try {
+                    // Sleep for 5 seconds
+                    Thread.sleep(2 * 1000);
+                } catch (InterruptedException e) {
+                    Log.d("TAG", "sleep failure");
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            super.onPostExecute(result);
+            mBuilder.setContentText("Download complete");
+            // Removes the progress bar
+            mBuilder.setProgress(0, 0, false);
+            mNotifyManager.notify(id, mBuilder.build());
+        }
+    }
+
     private void setPic() {
 
 		/* There isn't enough memory to open up more than a couple camera photos */
-		/* So pre-scale the target bitmap into which the file is decoded */
+        /* So pre-scale the target bitmap into which the file is decoded */
 
 		/* Get the size of the ImageView */
         int targetW = mImageView.getWidth();
@@ -112,14 +175,13 @@ public class Camera2Activity extends Activity {
 		/* Figure out which way needs to be reduced less */
         int scaleFactor = 1;
         if ((targetW > 0) || (targetH > 0)) {
-            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+            scaleFactor = Math.min(photoW / targetW, photoH / targetH);
         }
 
 		/* Set bitmap options to scale the image decode target */
         bmOptions.inJustDecodeBounds = false;
         bmOptions.inSampleSize = scaleFactor;
         bmOptions.inPurgeable = true;
-
 		/* Decode the JPEG file into a Bitmap */
         Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
 
@@ -142,7 +204,7 @@ public class Camera2Activity extends Activity {
 
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        switch(actionCode) {
+        switch (actionCode) {
             case ACTION_TAKE_PHOTO_B:
                 File f = null;
 
@@ -164,63 +226,28 @@ public class Camera2Activity extends Activity {
         startActivityForResult(takePictureIntent, actionCode);
     }
 
-    private void dispatchTakeVideoIntent() {
-        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-        startActivityForResult(takeVideoIntent, ACTION_TAKE_VIDEO);
-    }
-
-    private void handleSmallCameraPhoto(Intent intent) {
-        Bundle extras = intent.getExtras();
-        mImageBitmap = (Bitmap) extras.get("data");
-        mImageView.setImageBitmap(mImageBitmap);
-        mVideoUri = null;
-        mImageView.setVisibility(View.VISIBLE);
-        mVideoView.setVisibility(View.INVISIBLE);
-    }
-
     private void handleBigCameraPhoto() {
 
         if (mCurrentPhotoPath != null) {
             setPic();
             galleryAddPic();
+            startUpload(mCurrentPhotoPath);
             mCurrentPhotoPath = null;
         }
 
     }
 
-    private void handleCameraVideo(Intent intent) {
-        mVideoUri = intent.getData();
-        mVideoView.setVideoURI(mVideoUri);
-        mImageBitmap = null;
-        mVideoView.setVisibility(View.VISIBLE);
-        mImageView.setVisibility(View.INVISIBLE);
-    }
-
-    Button.OnClickListener mTakePicOnClickListener =
-            new Button.OnClickListener() {
+    ImageButton.OnClickListener mTakePicOnClickListener =
+            new ImageButton.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
                 }
             };
 
-    Button.OnClickListener mTakePicSOnClickListener =
-            new Button.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    dispatchTakePictureIntent(ACTION_TAKE_PHOTO_S);
-                }
-            };
-
-    Button.OnClickListener mTakeVidOnClickListener =
-            new Button.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    dispatchTakeVideoIntent();
-                }
-            };
-
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -231,30 +258,16 @@ public class Camera2Activity extends Activity {
         mImageBitmap = null;
         mVideoUri = null;
 
-        Button picBtn = (Button) findViewById(R.id.btnIntend);
+        ImageButton uploadBtn = (ImageButton) findViewById(R.id.btnUpload);
+        ImageButton picBtn = (ImageButton) findViewById(R.id.btnIntend);
         setBtnListenerOrDisable(
                 picBtn,
                 mTakePicOnClickListener,
                 MediaStore.ACTION_IMAGE_CAPTURE
         );
 
-        Button picSBtn = (Button) findViewById(R.id.btnIntendS);
-        setBtnListenerOrDisable(
-                picSBtn,
-                mTakePicSOnClickListener,
-                MediaStore.ACTION_IMAGE_CAPTURE
-        );
-
-        Button vidBtn = (Button) findViewById(R.id.btnIntendV);
-        setBtnListenerOrDisable(
-                vidBtn,
-                mTakeVidOnClickListener,
-                MediaStore.ACTION_VIDEO_CAPTURE
-        );
-
-
-            mAlbumStorageDirFactory = new BaseAlbumDirFactory();
-
+        mAlbumStorageDirFactory = new BaseAlbumDirFactory();
+        dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
     }
 
     @Override
@@ -267,19 +280,6 @@ public class Camera2Activity extends Activity {
                 break;
             } // ACTION_TAKE_PHOTO_B
 
-            case ACTION_TAKE_PHOTO_S: {
-                if (resultCode == RESULT_OK) {
-                    handleSmallCameraPhoto(data);
-                }
-                break;
-            } // ACTION_TAKE_PHOTO_S
-
-            case ACTION_TAKE_VIDEO: {
-                if (resultCode == RESULT_OK) {
-                    handleCameraVideo(data);
-                }
-                break;
-            } // ACTION_TAKE_VIDEO
         } // switch
     }
 
@@ -288,8 +288,8 @@ public class Camera2Activity extends Activity {
     protected void onSaveInstanceState(Bundle outState) {
         outState.putParcelable(BITMAP_STORAGE_KEY, mImageBitmap);
         outState.putParcelable(VIDEO_STORAGE_KEY, mVideoUri);
-        outState.putBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY, (mImageBitmap != null) );
-        outState.putBoolean(VIDEOVIEW_VISIBILITY_STORAGE_KEY, (mVideoUri != null) );
+        outState.putBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY, (mImageBitmap != null));
+        outState.putBoolean(VIDEOVIEW_VISIBILITY_STORAGE_KEY, (mVideoUri != null));
         super.onSaveInstanceState(outState);
     }
 
@@ -318,10 +318,9 @@ public class Camera2Activity extends Activity {
      * http://android-developers.blogspot.com/2009/01/can-i-use-this-intent.html
      *
      * @param context The application's environment.
-     * @param action The Intent action to check for availability.
-     *
+     * @param action  The Intent action to check for availability.
      * @return True if an Intent with the specified action can be sent and
-     *         responded to, false otherwise.
+     * responded to, false otherwise.
      */
     public static boolean isIntentAvailable(Context context, String action) {
         final PackageManager packageManager = context.getPackageManager();
@@ -333,15 +332,14 @@ public class Camera2Activity extends Activity {
     }
 
     private void setBtnListenerOrDisable(
-            Button btn,
-            Button.OnClickListener onClickListener,
+            ImageButton btn,
+            ImageButton.OnClickListener onClickListener,
             String intentName
     ) {
         if (isIntentAvailable(this, intentName)) {
             btn.setOnClickListener(onClickListener);
         } else {
-            btn.setText(
-                    getText(R.string.cannot).toString() + " " + btn.getText());
+
             btn.setClickable(false);
         }
     }
