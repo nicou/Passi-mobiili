468a5b26fbb62c3b4a6ff436a2a9ed92acffd8f1
Sami Holopainen <sami.roskaposti@gmail.com>
2016-09-15 10:25:42 +0300

    KemraButton uuteen layouttiin

--- app/src/main/java/fi/softala/passi/turvallisuuskavely_toteutus_2.java
+++ app/src/main/java/fi/softala/passi/turvallisuuskavely_toteutus_2.java
@@ -1,7 +1,10 @@
 package fi.softala.passi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class turvallisuuskavely_toteutus_2 extends ActionBarActivity {
 
@@ -9,6 +12,22 @@ public class turvallisuuskavely_toteutus_2 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_turvallisuuskavely_toteutus_2);
+
+        ImageButton cameraButton = (ImageButton) findViewById(R.id.button_kamera);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(turvallisuuskavely_toteutus_2.this, Camera2Activity.class);
+                startActivity(cameraIntent);
+            }
+        });
+    }
+    public void onBackPressed() {
+
+        super.onBackPressed();
+        Intent intent = new Intent(turvallisuuskavely_toteutus_2.this, TehtavakortinValintaActivity.class);
+        startActivity(intent);
+
     }
 
 }
