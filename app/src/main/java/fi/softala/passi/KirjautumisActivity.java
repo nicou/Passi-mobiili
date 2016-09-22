adcf562d55f4fc421e2f4bd64f27ee2a56852d71
Ville Aaltonen <ville.v.aaltonen@gmail.com>
2016-09-22 13:43:20 +0300

    Sisään kirjautuminen luo tilin kännykkään

--- app/src/main/java/fi/softala/passi/KirjautumisActivity.java
+++ app/src/main/java/fi/softala/passi/KirjautumisActivity.java
@@ -1,5 +1,7 @@
 package fi.softala.passi;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -19,6 +21,11 @@ import static fi.softala.passi.R.id.password;
  * Created by villeaaltonen on 15/09/16.
  */
 public class KirjautumisActivity extends Activity {
+    // Constants
+    // The authority for the sync adapter's content provider
+    public static final String AUTHORITY = "com.example.android.datasync.provider";
+    public static final String ACCOUNT_TYPE = "fi.softala.passi";
+    // An account type, in the form of a domain name
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,19 +74,26 @@ public class KirjautumisActivity extends Activity {
         return password.length() > 4;
     }
 
-    public void doLogin(String username,String password) {
+    public void doLogin(String username, final String password) {
         final ProgressDialog progressDialog = new ProgressDialog(KirjautumisActivity.this,
                 R.style.AppTheme_Dark_Dialog);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Kirjaudutaan...");
         progressDialog.show();
+        Context context = getApplicationContext();
+
+        // Get an instance of the Android account manager
+        final AccountManager accountManager =
+                (AccountManager) context.getSystemService(
+                        ACCOUNT_SERVICE);
         Log.d("Passi", username + " username " + password + " password");
         // TODO: Implement your own authentication logic here.
         if (username.equals("admin") && password.equals("admin")) {
+            Account account = new Account(username, ACCOUNT_TYPE);
+            accountManager.addAccountExplicitly(account, password, null);
             new android.os.Handler().postDelayed(
                     new Runnable() {
                         public void run() {
-
                             // On complete call either onLoginSuccess or onLoginFailed
                             onLoginSuccess();
                             // onLoginFailed();
@@ -90,7 +104,6 @@ public class KirjautumisActivity extends Activity {
             progressDialog.dismiss();
             int duration = Toast.LENGTH_LONG;
             String text = "Salasana tai käyttäjänimi väärin";
-            Context context = getApplicationContext();
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
         }
@@ -105,9 +118,11 @@ public class KirjautumisActivity extends Activity {
 
     }
 
+
     public void onLoginSuccess() {
         // btn.setEnabled(true);
-
+// Create the dummy account
+        // mAccount = CreateSyncAccount(this);
         Intent valikko = new Intent(KirjautumisActivity.this, ValikkoActivity.class);
         startActivity(valikko);
     }
--- app/src/main/java/fi/softala/passi/MainActivity.java
+++ app/src/main/java/fi/softala/passi/MainActivity.java
@@ -1,9 +1,12 @@
 package fi.softala.passi;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,7 +19,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Context context = getApplicationContext();
+        AccountManager accountManager =
+                (AccountManager) context.getSystemService(
+                        ACCOUNT_SERVICE);
+        Account[] tilit = accountManager.getAccountsByType("fi.softala.passi");
 
+        for (Account account : tilit) {
+            Log.e("Passi", account.name);
+        }
+        if (tilit != null && tilit.length > 0){
+            Intent intent = new Intent(MainActivity.this, ValikkoActivity.class);
+            startActivity(intent);
+        }
         Button button = (Button) findViewById(R.id.btnLogin);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
