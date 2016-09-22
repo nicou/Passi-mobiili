8576d58c156b837010eb751e2f637b523f6382b5
Ville Aaltonen <ville.v.aaltonen@gmail.com>
2016-09-22 15:26:22 +0300

    Otetaan polku talteen kuvasta

--- app/src/main/java/fi/softala/passi/Turvallisuuskavely_johdantoActivity.java
+++ app/src/main/java/fi/softala/passi/Turvallisuuskavely_johdantoActivity.java
@@ -1,10 +1,15 @@
 package fi.softala.passi;
 
+import android.content.ContentProvider;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -12,10 +17,15 @@ import android.widget.TabHost;
 import android.widget.TabWidget;
 import android.widget.TextView;
 
+import java.io.File;
+
 public class Turvallisuuskavely_johdantoActivity extends ActionBarActivity {
 
     TabHost tabHost;
-
+    File file;
+    Uri fileUri;
+    String stringUri;
+    final int RC_TAKE_PHOTO = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,13 +74,36 @@ public class Turvallisuuskavely_johdantoActivity extends ActionBarActivity {
         host.getTabWidget().getChildAt(host.getCurrentTab()). setBackgroundColor(Color.TRANSPARENT);
 
 
+
         TextView tv = (TextView) findViewById(R.id.textView1);
         tv.setMovementMethod(new ScrollingMovementMethod());
+        final ImageButton kameraButton = (ImageButton) findViewById(R.id.kameraButton1);
+        kameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent kameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                 file = new File(Turvallisuuskavely_johdantoActivity.this.getExternalCacheDir(),
+                         String.valueOf(System.currentTimeMillis() + ".jpg"));
+                 fileUri = Uri.fromFile(file);
+                kameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                Turvallisuuskavely_johdantoActivity.this.startActivityForResult(kameraIntent, RC_TAKE_PHOTO);
 
+            }
 
+        });
 
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Turvallisuuskavely_johdantoActivity.super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_TAKE_PHOTO && resultCode == RESULT_OK){
+            stringUri = fileUri.toString();
+            Log.e("Passi ", stringUri);
 
+        }
+    }
+
+    
 
 
 
