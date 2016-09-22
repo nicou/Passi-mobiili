32212bfb8b8895a23b7c560448dd1186c0466279
Ville Aaltonen <ville.v.aaltonen@gmail.com>
2016-09-22 16:48:28 +0300

    Vaihda camera kuva kun näppästy

--- app/src/main/java/fi/softala/passi/Turvallisuuskavely_johdantoActivity.java
+++ app/src/main/java/fi/softala/passi/Turvallisuuskavely_johdantoActivity.java
@@ -1,12 +1,23 @@
 package fi.softala.passi;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ContentProvider;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.provider.Settings;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -18,6 +29,18 @@ import android.widget.TabWidget;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Turvallisuuskavely_johdantoActivity extends ActionBarActivity {
 
@@ -25,7 +48,13 @@ public class Turvallisuuskavely_johdantoActivity extends ActionBarActivity {
     File file;
     Uri fileUri;
     String stringUri;
+    private String mCurrentPhotoPath;
+    private NotificationManager mNotifyManager;
+    private NotificationCompat.Builder mBuilder;
+    int id = 1;
     final int RC_TAKE_PHOTO = 1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,34 +106,180 @@ public class Turvallisuuskavely_johdantoActivity extends ActionBarActivity {
 
         TextView tv = (TextView) findViewById(R.id.textView1);
         tv.setMovementMethod(new ScrollingMovementMethod());
-        final ImageButton kameraButton = (ImageButton) findViewById(R.id.kameraButton1);
-        kameraButton.setOnClickListener(new View.OnClickListener() {
+
+        ImageButton kameraButton1 = (ImageButton) findViewById(R.id.kameraButton1);
+        ImageButton kameraButton2 = (ImageButton) findViewById(R.id.kameraButton2);
+        ImageButton kameraButton3 = (ImageButton) findViewById(R.id.kameraButton3);
+        ImageButton kameraButton4 = (ImageButton) findViewById(R.id.kameraButton4);
+        ImageButton kameraButton5 = (ImageButton) findViewById(R.id.kameraButton5);
+
+        kameraButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent kameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                 file = new File(Turvallisuuskavely_johdantoActivity.this.getExternalCacheDir(),
-                         String.valueOf(System.currentTimeMillis() + ".jpg"));
-                 fileUri = Uri.fromFile(file);
+                file = new File(Turvallisuuskavely_johdantoActivity.this.getExternalCacheDir(),
+                        String.valueOf(System.currentTimeMillis() + ".jpg"));
+                fileUri = Uri.fromFile(file);
                 kameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
                 Turvallisuuskavely_johdantoActivity.this.startActivityForResult(kameraIntent, RC_TAKE_PHOTO);
-
             }
-
         });
 
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Turvallisuuskavely_johdantoActivity.super.onActivityResult(requestCode, resultCode, data);
+        ImageButton kameraButton = (ImageButton) findViewById(R.id.kameraButton1);
+
         if (requestCode == RC_TAKE_PHOTO && resultCode == RESULT_OK){
             stringUri = fileUri.toString();
+            mCurrentPhotoPath = stringUri;
             Log.e("Passi ", stringUri);
-
+            Context context = getApplicationContext();
+            kameraButton.setBackground(ContextCompat.getDrawable(context, R.drawable.red_face_pressed)
+            );
+            kameraButton.setEnabled(false);
         }
     }
 
+    private void startUpload() {
+        Log.v("Passi", "polku kuvaan" + mCurrentPhotoPath);
+
+        mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mBuilder = new NotificationCompat.Builder(Turvallisuuskavely_johdantoActivity.this);
+        Intent untent = new Intent(Turvallisuuskavely_johdantoActivity.this, ValikkoActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                untent, 0);
+        mBuilder.setContentTitle("Vastaus")
+                .setContentText("Tallennetaan...")
+                .setSmallIcon(R.drawable.ic_cloud_upload_white_24dp)
+                .setContentIntent(pendingIntent);
 
 
+        new Turvallisuuskavely_johdantoActivity.UploadImage().execute(mCurrentPhotoPath);
+    }
+
+    private class UploadImage extends AsyncTask<String, Integer, Integer> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            // Displays the progress bar for the first time.
+            mBuilder.setProgress(100, 0, false);
+            mNotifyManager.notify(id, mBuilder.build());
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            // Update progress
+            mBuilder.setProgress(100, values[0], false);
+            mNotifyManager.notify(id, mBuilder.build());
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected Integer doInBackground(String... path) {
+            int i;
+
+            File image = saveBitmapToFile(file);
+            String url = "http://proto284.haaga-helia.fi/passibe/KuvaVastaanottoServlet";
+
+            OkHttpClient client = new OkHttpClient();
+
+            RequestBody formBody = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM).addFormDataPart("file", "image.png",
+                            RequestBody.create(MediaType.parse("image/png"), image))
+                    .build();
+
+            Request request = new Request.Builder().url(url).post(formBody).build();
+
+            Response response = null;
+
+            try {
+                response = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            int kahvi = 0;
+
+            Log.d("Passi", "Jee " + response);
+
+            if (response != null){
+                if (response.isSuccessful()) {
+                    kahvi = 1;
+                }
+            }
+
+            response.close();
+            for (i = 0; i <= 100; i += 5) {
+                // Sets the progress indicator completion percentage
+                publishProgress(Math.min(i, 100));
+
+            }
+            return kahvi;
+        }
+
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            Log.d("Passi", "Jee jöö " + result);
+            super.onPostExecute(result);
+            if (result == 1) {
+                mBuilder.setContentText("Vastaus tallennettu");
+            } else {
+                mBuilder.setContentText("Tallennus epäonnistui");
+            }
+            // Removes the progress bar
+            mBuilder.setProgress(0, 0, false);
+            mNotifyManager.notify(id, mBuilder.build());
+        }
+    }
+
+    public File saveBitmapToFile(File file) {
+        try {
+
+            // BitmapFactory options to downsize the image
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            o.inSampleSize = 6;
+            // factor of downsizing the image
+
+            FileInputStream inputStream = new FileInputStream(file);
+            //Bitmap selectedBitmap = null;
+            BitmapFactory.decodeStream(inputStream, null, o);
+            inputStream.close();
+
+            // The new size we want to scale to
+            final int REQUIRED_SIZE = 75;
+
+            // Find the correct scale value. It should be the power of 2.
+            int scale = 1;
+            while (o.outWidth / scale / 2 >= REQUIRED_SIZE &&
+                    o.outHeight / scale / 2 >= REQUIRED_SIZE) {
+                scale *= 2;
+            }
+
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+            inputStream = new FileInputStream(file);
+
+            Bitmap selectedBitmap = BitmapFactory.decodeStream(inputStream, null, o2);
+            inputStream.close();
+
+            // here i override the original image file
+            file.createNewFile();
+            FileOutputStream outputStream = new FileOutputStream(file);
+
+            selectedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+
+            return file;
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
 
 
