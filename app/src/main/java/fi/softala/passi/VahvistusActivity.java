53341ba6c8808f1b9003d73b00ca44613c783c3c
pepeto187 <a1400187@myy.haaga-helia.fi>
2016-10-06 16:08:36 +0300

    Combo_ukkeli vahvistussivulle ja buttonit kans

--- app/src/main/java/fi/softala/passi/VahvistusActivity.java
+++ app/src/main/java/fi/softala/passi/VahvistusActivity.java
@@ -1,7 +1,10 @@
 package fi.softala.passi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class VahvistusActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class VahvistusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vahvistus);
+        Button button = (Button) findViewById(R.id.btnPalaaKortteihin);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(VahvistusActivity.this, TehtavakortinValintaActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.btnTakaisinEtusivulle);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(VahvistusActivity.this, ValikkoActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
