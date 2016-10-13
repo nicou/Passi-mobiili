642d1dbd0f160d2b5e3ba324e1b0de7d8b3c1f19
Ville Aaltonen <ville.v.aaltonen@gmail.com>
2016-10-13 15:04:34 +0300

    Viilattu etappitekstiä

--- app/src/main/java/fi/softala/passi/adapters/KorttiAdapter.java
+++ app/src/main/java/fi/softala/passi/adapters/KorttiAdapter.java
@@ -4,12 +4,13 @@ package fi.softala.passi.adapters;
  * Created by 1 on 13/10/16.
  */
 
-        import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
         import android.widget.EditText;
-        import android.widget.TextView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
         import java.util.ArrayList;
         import java.util.List;
 
@@ -23,22 +24,19 @@ public class KorttiAdapter extends RecyclerView.Adapter<KorttiAdapter.ViewHolder
     View view1;
 
     public KorttiAdapter(List<WorksheetWaypoints> SubjectNames) {
-
         this.SubjectNames = SubjectNames;
     }
 
     @Override
     public KorttiAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-
         view1  = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.content_etappi, viewGroup, false);
-
         return new ViewHolder(view1);
     }
 
     @Override
     public void onBindViewHolder(KorttiAdapter.ViewHolder Viewholder, int i) {
         int id = SubjectNames.get(i).getWaypointID();
-        Viewholder.editText.setText(id + " " + SubjectNames.get(i).getWaypointTask());
+        Viewholder.editText.setText(id + ". " + SubjectNames.get(i).getWaypointTask());
     }
 
     @Override
