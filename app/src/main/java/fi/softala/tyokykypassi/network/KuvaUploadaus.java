6979e7a7a51760f79505f08d229efc36a3f5f69f
Ville Aaltonen <ville.v.aaltonen@gmail.com>
2016-11-22 14:14:42 +0200

    Muokattu kuvauploadausta

--- app/src/main/java/fi/softala/tyokykypassi/network/KuvaUploadaus.java
+++ app/src/main/java/fi/softala/tyokykypassi/network/KuvaUploadaus.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
-import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
