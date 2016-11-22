70a33bba0e2c4a547569648bd4107b9fddea4445
Kajander <joakim.kajan@myy.haaga-helia.fi>
2016-11-22 14:02:18 +0200

    Saarinäkymän ui hieromista ja liityRyhmaactivityyn edittext ja buttoni toimimaan

--- app/src/main/java/fi/softala/tyokykypassi/activities/LiityRyhmaActivity.java
+++ app/src/main/java/fi/softala/tyokykypassi/activities/LiityRyhmaActivity.java
@@ -2,6 +2,10 @@ package fi.softala.tyokykypassi.activities;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import fi.softala.tyokykypassi.R;
 
@@ -13,6 +17,19 @@ public class LiityRyhmaActivity extends ToolbarActivity {
         setContentView(R.layout.content_liity_ryhma);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        Button liityButton = (Button)findViewById(R.id.liityButton);
+        final EditText liityText   = (EditText)findViewById(R.id.liityText);
+
+        liityButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        Log.v("Ryhmätunnus: ", liityText.getText().toString());
+                    }
+                });
 
     }
 
