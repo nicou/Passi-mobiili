972839c2fdd011eb708e243ad8a7c4f7f0f52613
Ville Aaltonen <ville.v.aaltonen@gmail.com>
2016-11-29 14:35:58 +0200

    Lataa kuvista pienennetyt versiot kännykän muistiin

--- app/src/main/java/fi/softala/tyokykypassi/activities/LogotActivity.java
+++ app/src/main/java/fi/softala/tyokykypassi/activities/LogotActivity.java
@@ -3,9 +3,11 @@ package fi.softala.tyokykypassi.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import fi.softala.tyokykypassi.R;
+import fi.softala.tyokykypassi.utilities.ImageManipulation;
 
 public class LogotActivity extends ToolbarActivity {
 
@@ -32,6 +34,47 @@ public class LogotActivity extends ToolbarActivity {
 
         TextView logotext2 = (TextView) findViewById(R.id.logoteksti2);
         logotext2.setText("Sovelluksen sisällössä on hyödynnetty Alpo.fi-sivujen tehtäviä, jotka on aiemmin laadittu yhteistyössä seuraavien tahojen kanssa:");
+
+        ImageView haaga = (ImageView) findViewById(R.id.haaga_logo);
+        ImageView ttl = (ImageView) findViewById(R.id.ttl_logo);
+        ImageView saku = (ImageView) findViewById(R.id.saku_logo);
+        ImageView eu = (ImageView) findViewById(R.id.eu_logo);
+        ImageView ely = (ImageView) findViewById(R.id.ely_logo);
+        ImageView vipu = (ImageView) findViewById(R.id.vipuvoimaa_logo);
+        ImageView smart = (ImageView) findViewById(R.id.smart_moves_logo);
+        ImageView ehyt = (ImageView) findViewById(R.id.ehyt_logo);
+        ImageView mieli = (ImageView) findViewById(R.id.mieli_logo);
+        ImageView ye = (ImageView) findViewById(R.id.ye_logo);
+        ImageView sydan = (ImageView) findViewById(R.id.sydanliitto_logo);
+        ImageView nuorten = (ImageView) findViewById(R.id.nuorten_akatemia_logo);
+        ImageView tyokyky = (ImageView) findViewById(R.id.tyokykypassi_logo);
+
+        haaga.setImageBitmap(
+                ImageManipulation.decodeSampledBitmapFromResource(getResources(), R.drawable.kehittaja_haagahelia, 100, 70));
+        ttl.setImageBitmap(
+                ImageManipulation.decodeSampledBitmapFromResource(getResources(),R.drawable.kehittaja_ttl, 100, 70));
+        saku.setImageBitmap(
+                ImageManipulation.decodeSampledBitmapFromResource(getResources(), R.drawable.kehittaja_saku, 100, 70));
+        eu.setImageBitmap(
+                ImageManipulation.decodeSampledBitmapFromResource(getResources(), R.drawable.kehittaja_eu_esr, 100, 70));
+        ely.setImageBitmap(
+                ImageManipulation.decodeSampledBitmapFromResource(getResources(), R.drawable.kehittaja_ely, 100, 70));
+        vipu.setImageBitmap(
+                ImageManipulation.decodeSampledBitmapFromResource(getResources(), R.drawable.kehittaja_vipuvoimaa, 100, 70));
+        smart.setImageBitmap(
+                ImageManipulation.decodeSampledBitmapFromResource(getResources(), R.drawable.yhteistyo_smart_moves, 100, 70));
+        ehyt.setImageBitmap(
+                ImageManipulation.decodeSampledBitmapFromResource(getResources(), R.drawable.yhteistyo_ehyt, 100, 70));
+        mieli.setImageBitmap(
+                ImageManipulation.decodeSampledBitmapFromResource(getResources(), R.drawable.yhteistyo_mieli, 100, 70));
+        ye.setImageBitmap(
+                ImageManipulation.decodeSampledBitmapFromResource(getResources(), R.drawable.yhteistyo_ye, 100, 70));
+        sydan.setImageBitmap(
+                ImageManipulation.decodeSampledBitmapFromResource(getResources(), R.drawable.yhteistyo_sydanliitto, 100, 70));
+        nuorten.setImageBitmap(
+                ImageManipulation.decodeSampledBitmapFromResource(getResources(), R.drawable.yhteistyo_nuorten_akatemia, 100, 70));
+        tyokyky.setImageBitmap(
+                ImageManipulation.decodeSampledBitmapFromResource(getResources(), R.drawable.yhteistyo_tyokykypassi, 100, 70));
 
     }
 
